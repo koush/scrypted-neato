94bf14de5b96df54b1f8a2146b6036cb67d15791
Koushik Dutta <koushd@gmail.com>
2019-01-11 23:06:16 -0800

    neato support

--- src/main.js
+++ src/main.js
@@ -1,23 +1,114 @@
-import axios from 'axios';
+import "core-js/modules/es6.promise";
 
-log.i('Hello World');
-log.i('Babel polyfills Promise for us.');
-log.i('setTimeout is implemented by the Android host.');
+var botvac = require('node-botvac');
+var client = new botvac.Client();
 
-function wait(ms) {
-    return new Promise((resolve, reject) => {
-        setTimeout(resolve, ms);
+const username = scriptSettings.getString('username');
+const password = scriptSettings.getString('password');
+if (!username || !password) {
+    throw new Error('You must provide "username" and "password" values in your Script Settings.');
+}
+
+function Neato(robot) {
+    this.robot = robot;
+}
+
+Neato.prototype.isPausable = function () {
+    return true;
+}
+
+Neato.prototype.isRunning = function () {
+    return true;
+}
+
+Neato.prototype.isPaused = function () {
+    return true;
+}
+
+Neato.prototype.start = function () {
+    this.robot.getState(function (error, result) {
+        this.robot.startCleaning(function (data) {
+        });
+    }.bind(this));
+}
+
+Neato.prototype.dock = function () {
+    this.robot.getState(function (error, result) {
+        this.robot.sendToBase(function (data) {
+        });
+    }.bind(this));
+}
+
+Neato.prototype.pause = function () {
+    this.robot.getState(function (error, result) {
+        this.robot.pauseCleaning(function (data) {
+        });
+    }.bind(this));
+}
+
+Neato.prototype.stop = function () {
+    this.robot.getState(function (error, result) {
+        this.robot.stopCleaning(function (data) {
+        });
+    }.bind(this));
+}
+
+Neato.prototype.resume = function () {
+    this.robot.getState(function (error, result) {
+        this.robot.resumeCleaning(function (data) {
+        });
+    }.bind(this));
+}
+
+function DeviceProvider() {
+};
+
+DeviceProvider.prototype.getDevice = function (id) {
+    var robot = this.robots && this.robots[id];
+    if (robot)
+        return new Neato(robot);
+}
+
+DeviceProvider.prototype.updateRobots = function (robots) {
+    this.robots = {};
+    for (var robot of robots) {
+        this.robots[robot._serial] = robot;
+    }
+
+    var devices = robots.map(robot => {
+        return {
+            name: robot.name,
+            id: robot._serial,
+            interfaces: ['StartStop', 'Dock'],
+            type: 'Vacuum',
+        }
     })
+
+    deviceManager.onDevicesChanged({
+        devices
+    });
 }
 
-async function main() {
-    await wait(50);
-    log.i('done waiting for 50ms.');
-    log.i('XMLHttpRequest is polyfilled by the Android host. This allows the popular http library axios or jquery ajax to work.');
+var deviceProvider = new DeviceProvider();
 
-    const ip = await axios.get('http://jsonip.com');
-    log.i(`my ip: ${ip.data.ip}`);
-}
-main();
+//authorize
+client.authorize(username, password, false, function (error) {
+    if (error) {
+        throw error;
+    }
 
-log.i('The script will exit, but the async function will continue.');
+    //get your robots
+    client.getRobots(function (error, robots) {
+        if (error) {
+            console.log(error);
+            return;
+        }
+        var validRobots = robots
+            .filter(robot => robot._serial && robot._secret);
+
+        deviceProvider.updateRobots(validRobots);
+    });
+});
+
+
+exports.result = deviceProvider;
